f8027dc8792b6de236eaf8bd4d3a9e3756e04550
ViRaX5 <amit505r@gmail.com>
2026-05-30 19:22:31 +0300

    finished campaigns list page including js, need to link to sessions screens

--- js/campgain-list.js
+++ js/campgain-list.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const log_out = document.getElementsByClassName("log-out")[0];
-
-log_out.addEventListener('click', () => {
-    const confirm = window.confirm("Are you sure you want to log out?")
-
-    if(confirm)
-    {
-        window.location.href = "./index.html"
-    }
-});
-=======
 const log_out = document.getElementsByClassName("log-out")[0];
 const log_out_modal = document.getElementById("logout-modal");
 const cancel_logout_button = document.getElementById("cancel-logout");
@@ -24,6 +12,9 @@ const campaigns1 = [
     },
     {
         "campaign_name": "test2", "status": "DM", "amout_of_players": "5"
+    },
+    {
+        "campaign_name": "test3", "status": "player", "character_name": "Crash Bandicot"
     }
 ]
 const campaigns2 = []
@@ -31,115 +22,173 @@ const campaigns2 = []
 const campaigns = campaigns2
 
 const campaignContainerSection = document.querySelector('.campaigns-container')
+const noCampaignContainerSection = document.querySelector('.no-campaigns-container')
 /* no campaigns */
 if (campaigns.length === 0) {
-
-    const messageContainer = document.createElement('div')
-    const buttonsContainer = document.createElement('div')
-    const heading = document.createElement('h3')
-    const message = document.createElement('p')
-    const createButton = document.createElement('button')
-    const joinButton = document.createElement('button')
-
-    messageContainer.classList.add("empty-state-message")
-    buttonsContainer.classList.add("buttons-container")
-    createButton.classList.add("create-button")
-    joinButton.classList.add("join-button")
-    heading.innerText = "You have no active campaigns"
-    message.innerText = "It seems your adventure hasn't started yet\nJoin a party as a player or create new world as a DM!\nYour journey awaits!"
-    createButton.innerText = "Create Campaign"
-    joinButton.innerText = "Join Campaign"
-
-    messageContainer.appendChild(heading)
-    messageContainer.appendChild(message)
-    buttonsContainer.appendChild(joinButton)
-    buttonsContainer.appendChild(createButton)
-    messageContainer.appendChild(buttonsContainer)
-
-    campaignContainerSection.appendChild(messageContainer)
+    noSessions()
 }
 else {
+    noCampaignContainerSection.style.display = 'none'
     campaigns.forEach(campaign => {
         if (campaign.status === "player") {
-            const campaignName = campaign.campaign_name
-            const status = campaign.status
-            const characterName = campaign.character_name
-
-            const campaignInstance = document.createElement('div')
-            const buttonsContainer = document.createElement('div')
-            const campaignNameSpan = document.createElement('span')
-            const statusSpan = document.createElement('span')
-            const nameSpan = document.createElement('span')
-            const joinButton = document.createElement('button')
-            const abandonButton = document.createElement('button')
-
-            campaignInstance.classList.add('campaign-instance')
-            buttonsContainer.classList.add("instance-buttons-container")
-            joinButton.classList.add('join-button')
-            abandonButton.classList.add('abandon-button')
-
-            campaignNameSpan.innerText = `Campaign name: ${campaignName}`
-            statusSpan.innerText = `Status: ${status}`
-            nameSpan.innerText = `Character's name: ${characterName}`
-            joinButton.innerText = "Join Session"
-            abandonButton.innerText = "Abandon Campaign"
-
-            campaignInstance.appendChild(campaignNameSpan)
-            campaignInstance.appendChild(statusSpan)
-            campaignInstance.appendChild(nameSpan)
-            buttonsContainer.appendChild(joinButton)
-            buttonsContainer.appendChild(abandonButton)
-            campaignInstance.appendChild(buttonsContainer)
-
-            campaignContainerSection.appendChild(campaignInstance)
+            displayPlayerSessions(campaign)
         }
         else if (campaign.status === "DM") {
-            const campaignName = campaign.campaign_name
-            const status = campaign.status
-            const playerCount = campaign.amout_of_players
-
-            const campaignInstance = document.createElement('div')
-            const buttonsContainer = document.createElement('div')
-            const campaignNameSpan = document.createElement('span')
-            const statusSpan = document.createElement('span')
-            const playerCountSpan = document.createElement('span')
-            const startSessionButton = document.createElement('button')
-            const abandonButton = document.createElement('button')
-
-            campaignInstance.classList.add('campaign-instance')
-            buttonsContainer.classList.add("instance-buttons-container")
-            startSessionButton.classList.add('start-session-button')
-            abandonButton.classList.add('abandon-button')
-
-            campaignNameSpan.innerText = `Campaign name: ${campaignName}`
-            statusSpan.innerText = `Status: ${status}`
-            playerCountSpan.innerText = `Amount of players: ${playerCount}`
-            startSessionButton.innerText = "Start Session"
-            abandonButton.innerText = "Abandon Campaign"
-
-            campaignInstance.appendChild(campaignNameSpan)
-            campaignInstance.appendChild(statusSpan)
-            campaignInstance.appendChild(playerCountSpan)
-            campaignInstance.appendChild(buttonsContainer)
-            buttonsContainer.appendChild(startSessionButton)
-            buttonsContainer.appendChild(abandonButton)
-
-            campaignContainerSection.appendChild(campaignInstance)
-
+            displayDMSessions(campaign)
         }
         else {
             console.log("NOT A PLAYER OR A DM!!!")
         }
     });
+    addButtons(campaignContainerSection)
 }
 
+function addButtons(location) {
+    const buttonsContainer = document.createElement('div')
+    const createButton = document.createElement('button')
+    const joinButton = document.createElement('button')
+    buttonsContainer.classList.add("buttons-container")
+    createButton.classList.add("create-button")
+    joinButton.classList.add("join-button")
+    createButton.innerText = "Create Campaign"
+    joinButton.innerText = "Join Campaign"
+    buttonsContainer.appendChild(joinButton)
+    buttonsContainer.appendChild(createButton)
+    location.appendChild(buttonsContainer)
+}
+
+function noSessions() {
+    const noSessionsContainer = document.createElement('div')
+    const messageContainer = document.createElement('div')
+    const heading = document.createElement('h3')
+    const message = document.createElement('p')
+
+    noSessionsContainer.classList.add("no-session-container")
+    messageContainer.classList.add("empty-state-message")
+    heading.innerText = "You have no active campaigns"
+    message.innerText = "It seems your adventure hasn't started yet\nJoin a party as a player or create new world as a DM!\nYour journey awaits!"
+
+    messageContainer.appendChild(heading)
+    messageContainer.appendChild(message)
+    addButtons(messageContainer)
+    noSessionsContainer.appendChild(messageContainer)
+
+    campaignContainerSection.style.display = 'none'
+
+    noCampaignContainerSection.appendChild(noSessionsContainer)
+}
+
+function displayDMSessions(campaign) {
+    const campaignName = campaign.campaign_name
+    const status = campaign.status
+    const playerCount = campaign.amout_of_players
+
+    const campaignInstance = document.createElement('div')
+    const buttonsContainer = document.createElement('div')
+    const campaignNameSpan = document.createElement('span')
+    const statusSpan = document.createElement('span')
+    const playerCountSpan = document.createElement('span')
+    const startSessionButton = document.createElement('button')
+    const abandonButton = document.createElement('button')
+
+    campaignInstance.classList.add('campaign-instance', 'dm-campaign')
+    buttonsContainer.classList.add("instance-buttons-container")
+    startSessionButton.classList.add('start-session-button')
+    abandonButton.classList.add('abandon-button')
+
+    campaignNameSpan.innerText = `Campaign name: ${campaignName}`
+    statusSpan.innerText = `Status: ${status}`
+    playerCountSpan.innerText = `Amount of players: ${playerCount}`
+    startSessionButton.innerText = "Host Session"
+    abandonButton.innerText = "Abandon Campaign"
+
+    campaignInstance.appendChild(campaignNameSpan)
+    campaignInstance.appendChild(statusSpan)
+    campaignInstance.appendChild(playerCountSpan)
+    campaignInstance.appendChild(buttonsContainer)
+    buttonsContainer.appendChild(startSessionButton)
+    buttonsContainer.appendChild(abandonButton)
+
+    campaignContainerSection.appendChild(campaignInstance)
+}
+
+function displayPlayerSessions(campaign) {
+    const campaignName = campaign.campaign_name
+    const status = campaign.status
+    const characterName = campaign.character_name
+
+    const campaignInstance = document.createElement('div')
+    const buttonsContainer = document.createElement('div')
+    const campaignNameSpan = document.createElement('span')
+    const statusSpan = document.createElement('span')
+    const nameSpan = document.createElement('span')
+    const joinButton = document.createElement('button')
+    const abandonButton = document.createElement('button')
+
+    campaignInstance.classList.add('campaign-instance', 'player-campaign')
+    buttonsContainer.classList.add("instance-buttons-container")
+    joinButton.classList.add('join-button')
+    abandonButton.classList.add('abandon-button')
+
+    campaignNameSpan.innerText = `Campaign name: ${campaignName}`
+    statusSpan.innerText = `Status: ${status}`
+    nameSpan.innerText = `Character's name: ${characterName}`
+    joinButton.innerText = "Join Session"
+    abandonButton.innerText = "Abandon Campaign"
+
+    campaignInstance.appendChild(campaignNameSpan)
+    campaignInstance.appendChild(statusSpan)
+    campaignInstance.appendChild(nameSpan)
+    buttonsContainer.appendChild(joinButton)
+    buttonsContainer.appendChild(abandonButton)
+    campaignInstance.appendChild(buttonsContainer)
+
+    campaignContainerSection.appendChild(campaignInstance)
+}
+
+const dmCampaigns = document.querySelectorAll('.dm-campaign')
+const playerCampaigns = document.querySelectorAll('.player-campaign')
 
 player_button.addEventListener('click', () => {
+
+    if (player_button.classList.contains('not-clicked')) {
+        if (dm_button.classList.contains('clicked')) {
+            dm_button.classList.remove('clicked')
+            dm_button.classList.add('not-clicked')
+        }
+        player_button.classList.remove('not-clicked')
+        player_button.classList.add('clicked')
+
+        dmCampaigns.forEach(campaign => campaign.style.display = 'none')
+        playerCampaigns.forEach(campaign => campaign.style.display = '')
+    }
+    else {
+        player_button.classList.remove('clicked')
+        player_button.classList.add('not-clicked')
+        dmCampaigns.forEach(campaign => campaign.style.display = '')
+    }
 
     player_button.blur()            //will probably need to chage it later to what it takes to
 })
 
 dm_button.addEventListener('click', () => {
+
+    if (dm_button.classList.contains('not-clicked')) {
+        if (player_button.classList.contains('clicked')) {
+            player_button.classList.remove('clicked')
+            player_button.classList.add('not-clicked')
+        }
+        dm_button.classList.remove('not-clicked')
+        dm_button.classList.add('clicked')
+
+        playerCampaigns.forEach(campaign => campaign.style.display = 'none')
+        dmCampaigns.forEach(campaign => campaign.style.display = '')
+    }
+    else {
+        dm_button.classList.remove('clicked')
+        dm_button.classList.add('not-clicked')
+        playerCampaigns.forEach(campaign => campaign.style.display = '')
+    }
 
     dm_button.blur()                //will probably need to chage it later to what it takes to
 })
@@ -162,4 +211,3 @@ confirm_logout_button.addEventListener('click', () => {
     /* some save to database logic that needs to come in the future */
     window.location.href = "index.html"
 })
->>>>>>> 5930b3796c808e120629b3f622a68a9694173a5b
